7e63d16331f6f6f36fed15d202881fa0c2279d57
ThBraud <thomas.braud04@gmail.com>
2025-10-31 14:25:09 +0100

    commit

--- obj/Debug/net9.0/Projet_Finale.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projet_Finale.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_Finale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+274cdb291672d3eb21ae6ef6c6f71a4d6bedfc57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55077709b9c40785c8b127841eabbe5987eb1524")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_Finale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_Finale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
