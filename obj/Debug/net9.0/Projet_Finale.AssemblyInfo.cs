40a1780d088ff28133a6801777cda3fbb0dcf015
Lucas MOLENDI <lucasmolendi0@gmail.com>
2025-10-31 11:14:47 +0100

    nugget

--- obj/Debug/net9.0/Projet_Finale.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projet_Finale.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_Finale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4298715f29485c1e0df5f15d663352ac8c16df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfcacef84adb500e9ca8523c9755b857d358f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_Finale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_Finale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
