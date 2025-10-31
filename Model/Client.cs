e4c46af5c73a02789d8a6d3576f4aa5c45dd1f78
Lucas MOLENDI <lucasmolendi0@gmail.com>
2025-10-31 10:24:47 +0100

    ajout Client.cs

---
+++ Model/Client.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Projet_Finale.Model;
+
+public class Client
+{
+    [key]
+    public uuid id_client { get; set; } = new.guid();
+    
+    [Required]
+    private string FirstName {get; set;}
+    
+    [Required]
+    private string LastName {get; set;}
+    
+    [Required]
+    private DateTime BirthDate {get; set;}
+    
+    [Required]
+    private int PhoneNumber {get; set;}
+    
+    [Required]
+    private string Email {get; set;}
+    
+}
